48b67b5c39d294205136d14e09ec0ae2ac3f8d25
Jonathan Zambrano <deox.desing@gmail.com>
2023-12-16 16:49:14 -0500

    testing rama

--- fortiGuard-App/src/app/page/dashboard/dashboard.component.ts
+++ fortiGuard-App/src/app/page/dashboard/dashboard.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HeaderUserComponent } from '../../components/header-user/header-user.component';
-import { LoginComponent } from '../../page/login/login.component'
+import { LoginComponent } from '../../page/login/login.component';
 import { Router } from '@angular/router';
-import { HomeDashboardComponent } from '../../components/home/home.component'
-import { DashboardComponentApp } from '../../components/dashboard/dashboard.component'
-import { OrdersComponent } from '../../components/orders/orders.component'
-import { ProductsComponent } from '../../components/products/products.component'
-import { AppsComponent } from '../../components/apps/apps.component'
-import { HttpClient } from '@angular/common/http'
-import { CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule
-
+import { HomeDashboardComponent } from '../../components/home/home.component';
+import { DashboardComponentApp } from '../../components/dashboard/dashboard.component';
+import { OrdersComponent } from '../../components/orders/orders.component';
+import { ProductsComponent } from '../../components/products/products.component';
+import { AppsComponent } from '../../components/apps/apps.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -30,66 +29,30 @@ import { CommonModule } from '@angular/common'; // Asegúrate de importar Common
   styleUrl: 'dashboard.component.css'
 })
 
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent implements OnInit, OnDestroy {
   authToken: string = '';
   decodedToken: any = null;
-  userId: string = '';  // Declarar la propiedad userId
+  userId: string = '';
   userAccount: string = '';
-  userEmail: string = '';  // Declarar la propiedad userEmail
-  rol: string = '';  // Declarar la propiedad rol
-  createDate: string = '';  // Declarar la propiedad createDate
+  userEmail: string = '';
+  rol: string = '';
+  createDate: string = '';
+  isServer: boolean = false;  // Agrega esta línea para almacenar si estás en el lado del servidor
+
 
   constructor(
     private router: Router,
     private httpClient: HttpClient,
-
-  ) { }
-
-  ngOnInit() {
-    // Verificar si localStorage está disponible
-    if (typeof localStorage !== 'undefined') {
-      // Recuperar el token del localStorage al inicializar el componente
-      this.authToken = localStorage.getItem('authToken') || '';
-
-      if (this.authToken) {
-        const decodedToken = this.decodeToken(this.authToken);
-
-        // Asignar valores a variables
-        const userId = decodedToken.userId;
-        const userAccount = decodedToken.userAccount;
-        const userEmail = decodedToken.userEmail;
-        const rol = decodedToken.rol;
-        const createDate = decodedToken.createDate;
-
-        // Asignar estas variables a propiedades de tu componente
-        this.userId = userId;
-        this.userAccount = userAccount;
-        this.userEmail = userEmail;
-        this.rol = rol;
-        this.createDate = createDate;
-      } else {
-        // Manejar la situación en la que el token no está presente
-        console.error('Token no encontrado en el localStorage.');
-
-        // Redirigir al usuario a la página de inicio de sesión
-        this.router.navigate(['/login']);
-      }
-    } else {
-      // Manejar la situación en la que localStorage no está disponible
-      console.error('localStorage no está disponible en este entorno.');
-
-      // Redirigir al usuario a la página de inicio de sesión u otra acción según sea necesario
-      this.router.navigate(['/login']);
-    }
+  ) {
+    this.isServer = typeof localStorage === 'undefined';
   }
 
+  ngOnInit() {
+    this.verificarToken();
+  }
 
-  private decodeToken(token: string): any {
+  public decodeToken(token: string): any {
     try {
-      // Decodificar el token sin usar jwt_decode
-      // Aquí implementarías tu lógica de decodificación manual del token
-      // Por ejemplo, podrías dividir el token por puntos y decodificar la parte que contiene la información
       const tokenParts = token.split('.');
       const decodedToken = JSON.parse(atob(tokenParts[1]));
       return decodedToken;
@@ -99,28 +62,48 @@ export class DashboardComponent implements OnInit {
     }
   }
 
-
-  destroyToken() {
-    // Obtener el token del localStorage
+  public destroyToken(): void {
     const authToken = localStorage.getItem('authToken');
 
-    // Verificar si el token existe
-    if (authToken) {
-      // Eliminar el token del localStorage
-      localStorage.removeItem('authToken');
+    const headers = new HttpHeaders({
+      'Authorization': authToken || '',
+      'Content-Type': 'application/json'
+    });
 
-      // Hacer una solicitud al backend para cerrar sesión
-      this.httpClient.post<any>('http://localhost:3000/api/login/logout', {})
-        .subscribe(
-          (response) => {
-            console.log('Respuesta de la API (logout):', response);
-          },
-          (error) => {
-            console.error('Error al hacer logout en el backend:', error);
-          }
-        );
-    }
-    // Redirigir al usuario a la página de inicio de sesión
-    this.router.navigate(['/login']);
+    this.httpClient.post<any>('http://localhost:3000/api/login/logout', {}, { headers })
+      .subscribe(
+        (response) => {
+          console.log('Respuesta de la API (logout):', response);
+
+          localStorage.removeItem('authToken');
+
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Error al hacer logout en el backend:', error);
+        }
+      );
   }
+
+  public verificarToken(): void {
+    const authToken = localStorage.getItem('authToken') || '';
+
+    if (authToken) {
+
+      this.decodedToken = this.decodeToken(authToken);
+      this.userId = this.decodedToken.userId;
+      this.userAccount = this.decodedToken.userAccount;
+      this.userEmail = this.decodedToken.userEmail;
+      this.rol = this.decodedToken.rol;
+      this.createDate = this.decodedToken.createDate;
+    } else {
+      console.error('Token no encontrado en el localStorage.');
+      this.router.navigate(['/login']);
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroyToken()
+  }
+
 }
--- fortiGuard-App/src/app/page/profile/profile.component.ts
+++ fortiGuard-App/src/app/page/profile/profile.component.ts
@@ -10,12 +10,13 @@ import { ProfileComponentUser } from '../../components/profile/profile.component
   standalone: true,
   imports: [
     HeaderUserComponent,
-    ProfileComponentUser
+    ProfileComponentUser,
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
+
   authToken: string = '';
   userId: string = '';
   userAccount: string = '';
@@ -23,64 +24,26 @@ export class ProfileComponent implements OnInit {
   rol: string = '';
   createDate: string = '';
 
-  constructor(private httpClient: HttpClient, private router: Router) {
-    // Llama a la función de verificación de token en el constructor
-    this.verificarToken();
-  }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+    private dashboardComponent: DashboardComponent
+    ) {
+      
+     }
 
   ngOnInit(): void {
     // Puedes llamar a la función de verificación de token aquí si lo prefieres
-    // this.verificarToken();
+    this.dashboardComponent.verificarToken()
   }
 
-  cerrarSesion(): void {
-    // Llama a la función destroyToken del DashboardComponent
-    const dashboardComponent = new DashboardComponent(this.router, this.httpClient);
-    dashboardComponent.destroyToken();
+  destroyToken(): void {
+    this.dashboardComponent.destroyToken();
   }
 
-  private verificarToken(): void {
-    // Verificar si localStorage está disponible
-    if (typeof localStorage !== 'undefined') {
-      // Recuperar el token del localStorage al inicializar el componente
-      this.authToken = localStorage.getItem('authToken') || '';
+    
 
-      if (this.authToken) {
-        const decodedToken = this.decodeToken(this.authToken);
 
-        // Asignar valores a variables
-        this.userId = decodedToken.userId;
-        this.userAccount = decodedToken.userAccount;
-        this.userEmail = decodedToken.userEmail;
-        this.rol = decodedToken.rol;
-        this.createDate = decodedToken.createDate;
-      } else {
-        // Manejar la situación en la que el token no está presente
-        console.error('Token no encontrado en el localStorage.');
 
-        // Redirigir al usuario a la página de inicio de sesión
-        this.router.navigate(['/login']);
-      }
-    } else {
-      // Manejar la situación en la que localStorage no está disponible
-      console.error('localStorage no está disponible en este entorno.');
-
-      // Redirigir al usuario a la página de inicio de sesión u otra acción según sea necesario
-      this.router.navigate(['/login']);
-    }
-  }
-
-  private decodeToken(token: string): any {
-    try {
-      // Decodificar el token sin usar jwt_decode
-      // Aquí implementarías tu lógica de decodificación manual del token
-      // Por ejemplo, podrías dividir el token por puntos y decodificar la parte que contiene la información
-      const tokenParts = token.split('.');
-      const decodedToken = JSON.parse(atob(tokenParts[1]));
-      return decodedToken;
-    } catch (error) {
-      console.error('Error al decodificar el token:', error);
-      return null;
-    }
-  }
 }
+
